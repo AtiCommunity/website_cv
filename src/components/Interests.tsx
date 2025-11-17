da06883b323a772e2dbab24774bd031c7b11619f
Antoine ROBIN <antoine.robin@aticommunity.fr>
2025-11-17 14:02:21 +0100

    feat(cv): update skills and interests

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -1,17 +1,15 @@
 import PointOfInterest from "./Interests/point-of-interest";
 import Projects from "./Interests/projects";
-import Sports from "./Interests/sports";
 
 const Interests = () => {
-    return (
-        <>
-            <div className="container flex flex-col xl:flex-row xl:justify-around items-center p-5 max-w-7xl bg-black/40 border-0 rounded-box">
-                <Projects />
-                <PointOfInterest />
-                <Sports />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="container flex flex-col xl:flex-row xl:justify-around items-center p-5 max-w-7xl bg-black/40 border-0 rounded-box">
+        <Projects />
+        <PointOfInterest />
+      </div>
+    </>
+  );
 };
 
 export default Interests;
--- src/components/Interests/point-of-interest.tsx
+++ src/components/Interests/point-of-interest.tsx
@@ -3,26 +3,36 @@ import { t } from "i18next";
 import { FaHandPointUp } from "react-icons/fa6";
 
 const PointOfInterest = () => {
-    return (
-        <>
-            <div className="flex flex-col gap-10 p-5">
-                <div className="flex items-center justify-center gap-2">
-                    <FaHandPointUp size={25} />
-                    <h1 className="title">{t("point_of_interest_title")}</h1>
-                </div>
-                <div className="flex flex-col text-center gap-6">
-                    <div className="flex flex-col">
-                        <p className="text-green-500">
-                            {t("point_of_interest_1_title")}
-                        </p>
-                        <p className="text-sm italic">
-                            {t("point_of_interest_1_description")}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-col gap-10 p-5">
+        <div className="flex items-center justify-center gap-2">
+          <FaHandPointUp size={25} />
+          <h1 className="title">{t("point_of_interest_title")}</h1>
+        </div>
+        <div className="flex flex-col text-center gap-6">
+          <div className="flex flex-col">
+            <p className="text-green-500">{t("point_of_interest_1_title")}</p>
+            <p className="text-sm italic">
+              {t("point_of_interest_1_description")}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-green-500">{t("point_of_interest_2_title")}</p>
+            <p className="text-sm italic">
+              {t("point_of_interest_2_description")}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-green-500">{t("point_of_interest_3_title")}</p>
+            <p className="text-sm italic">
+              {t("point_of_interest_3_description")}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PointOfInterest;
